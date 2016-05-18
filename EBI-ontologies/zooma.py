a255c64a0a425f207ecc2ffa4dc4ea5e5189e87e
LLTommy <Thomas.Liener@felix-solution.com>
2016-05-18 22:34:40 +0100

    Starting with the zooma webservice

--- EBI-ontologies/zooma.py
+++ EBI-ontologies/zooma.py
@@ -1,1 +1,48 @@
 #Just an idea how to structure the code - here we could add the zooma webservice call stuff
+import urllib
+import json
+
+baseURL="http://www.ebi.ac.uk/spot/zooma/v2/api"
+
+class annotation:
+    def __init__(self,confidence, uri, propertyValue, weblink):
+        self.confidence=confidence
+        self.uri=uri
+        self.propertyValue=propertyValue
+        self.weblink=weblink
+
+def callZooma(searchurl):
+    try:
+        request=searchurl
+        reply = urllib.urlopen(request)
+        anwser = json.load(reply)
+        return anwser
+    except:
+        raise LookupError('Failed to reach the Endpoint, Service is down or wrong url!')
+
+
+def predictAnnotation(value):
+    url=baseURL+"/services/annotate?propertyValue="
+    url=url+value
+    anwser=callZooma(url)
+    return anwser
+
+def parseAnnotation(annotationTerm):
+    confidence=annotationTerm[0]["confidence"]
+    if (annotationTerm[0]["annotatedProperty"]["uri"]!=None):
+        uri=annotationTerm[0]["annotatedProperty"]["uri"]
+    else:
+        uri="Not available"
+    propertyValue=annotationTerm[0]["annotatedProperty"]["propertyValue"]
+    weblink=baseURL+"/services/annotate?propertyValue="+propertyValue
+    return annotation(confidence, uri, propertyValue, weblink)
+
+def showAnnotation(annotationTerm):
+    print("------------------------------------------")
+    print("confidence: "+annotationTerm.confidence)
+    print("uri: "+annotationTerm.uri)
+    print("propertyValue: "+annotationTerm.propertyValue)
+    print("weblink: "+annotationTerm.weblink)
+    print("------------------------------------------")
+
+showAnnotation(parseAnnotation(predictAnnotation("mus+musculus")))
